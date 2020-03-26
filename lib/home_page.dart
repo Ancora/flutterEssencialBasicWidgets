e7ce14d28222493ce6b00c52eef0525088959e34
Ancora <anderson.rocha@ancora-ti.com.br>
2020-03-26 19:34:44 -0300

    053-MaterialApp/Widgets ListView I.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterbasicwidgets/pages/hello_listview.dart';
 import 'package:flutterbasicwidgets/pages/hello_page1.dart';
 import 'package:flutterbasicwidgets/pages/hello_page2.dart';
 import 'package:flutterbasicwidgets/pages/hello_page3.dart';
@@ -55,7 +56,7 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             BlueButton('ListView',
-                onPressed: () => _onClickNavigator(context, HelloPage1())),
+                onPressed: () => _onClickNavigator(context, HelloListView())),
             BlueButton('Page 2',
                 onPressed: () => _onClickNavigator(context, HelloPage2())),
             BlueButton('Page 3',
---
+++ lib/pages/hello_listview.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HelloListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ListView'),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+      color: Colors.lightBlueAccent,
+      child: ListView(
+        children: <Widget>[
+          _img('assets/images/black-german.jpg'),
+          _img('assets/images/dog.jpg'),
+          _img('assets/images/husky.jpg'),
+          _img('assets/images/pug.jpg'),
+          _img('assets/images/rottweiler.jpg'),
+          _img('assets/images/schafer.jpg'),
+        ],
+      ),
+    );
+  }
+
+  _img(String img) {
+    return Image.asset(
+      img,
+      fit: BoxFit.cover,
+    );
+  }
+}

24e206f9ab7f70ab94c83749684cf66f9368a3b5
Ancora <anderson.rocha@ancora-ti.com.br>
2020-03-26 21:09:12 -0300

    057-MaterialApp/Wdigets/ListView IV objetos.

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+class Dog {
+  String nome;
+  String foto;
+
+  Dog(this.nome, this.foto);
+}
+
 class HelloListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,22 +19,23 @@ class HelloListView extends StatelessWidget {
   }
 
   _body() {
-    List<Image> imgs = [
-      _img('assets/images/black-german.jpg'),
-      _img('assets/images/dog.jpg'),
-      _img('assets/images/husky.jpg'),
-      _img('assets/images/pug.jpg'),
-      _img('assets/images/rottweiler.jpg'),
-      _img('assets/images/schafer.jpg'),
+    List<Dog> dogs = [
+      Dog('Pastor Alemão Negro', 'assets/images/black-german.jpg'),
+      Dog('Cão Negro', 'assets/images/dog.jpg'),
+      Dog('Husky Siberiano', 'assets/images/husky.jpg'),
+      Dog('Pug', 'assets/images/pug.jpg'),
+      Dog('Rottweiler', 'assets/images/rottweiler.jpg'),
+      Dog('Pastor Alemão', 'assets/images/schafer.jpg'),
     ];
 
     return Container(
       color: Colors.lightBlueAccent,
       child: ListView.builder(
-        itemCount: imgs.length,
+        itemCount: dogs.length,
         itemExtent: 300,
         itemBuilder: (context, index) {
-          return imgs[index];
+          Dog dog = dogs[index];
+          return _img(dog.foto);
         },
       ),
     );
